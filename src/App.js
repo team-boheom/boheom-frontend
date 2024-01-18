28f1253d655b9e04ffc37fe522a5f4808c40162b
juwon <juwon2298@naver.com>
2024-01-18 23:40:17 +0900

    🔒 :: App 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
-// import { globalStyle } from './styles/GlobalStyle';
+import { globalStyle } from './styles/GlobalStyle';
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { RecoilRoot } from 'recoil';
-import { Logo } from './assets';
 import { theme } from './styles/theme';
-import styled from '@emotion/styled';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,12 +21,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        {/* <Global styles={globalStyle} /> */}
+        <Global styles={globalStyle} />
         <RecoilRoot>
           <Toaster />
           <BrowserRouter>
-            <img src={Logo} />
-            {/* <MainRouter /> */}
+            <MainRouter />
           </BrowserRouter>
         </RecoilRoot>
       </ThemeProvider>
