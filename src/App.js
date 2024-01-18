39e6b2df54cec796644594cbe16e7478ed755897
KANGYONGSU23 <edc20203@gmail.com>
2024-01-18 23:18:20 +0900

    ✨ :: theme 제공

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 // import { globalStyle } from './styles/GlobalStyle';
-// import { Global, ThemeProvider } from '@emotion/react';
-import { RecoilRoot } from 'recoil';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-// import { theme } from './styles/Theme';
+import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+import { RecoilRoot } from 'recoil';
 import { Logo } from './assets';
+import { theme } from './styles/theme';
+import styled from '@emotion/styled';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,16 +22,16 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ThemeProvider theme={theme}>
-        <Global styles={globalStyle} /> */}
-      <RecoilRoot>
-        <Toaster />
-        <BrowserRouter>
-          <img src={Logo} />
-          {/* <MainRouter /> */}
-        </BrowserRouter>
-      </RecoilRoot>
-      {/* </ThemeProvider> */}
+      <ThemeProvider theme={theme}>
+        {/* <Global styles={globalStyle} /> */}
+        <RecoilRoot>
+          <Toaster />
+          <BrowserRouter>
+            <img src={Logo} />
+            {/* <MainRouter /> */}
+          </BrowserRouter>
+        </RecoilRoot>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
