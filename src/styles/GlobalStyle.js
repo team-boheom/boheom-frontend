61caedb4198185210079479a032cef42938061db
juwon <juwon2298@naver.com>
2024-01-18 23:37:34 +0900

    💄:: GlobalStyle 설정

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+export const globalStyle = css`
+  * {
+    font-family: 'Pretendard', sans-serif !important;
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+    -webkit-touch-callout: none;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
