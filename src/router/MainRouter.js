94f128ca5fbaca5970930dd59e1b3e7bd7ac6763
juwon <juwon2298@naver.com>
2024-01-18 23:39:06 +0900

    ✨:: Router 설정

---
+++ src/router/MainRouter.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+const mainRouter = () => {
+  return (
+    <Routes>
+      <Route /> {/* 페이지 맞게 알잘딱 */}
+    </Routes>
+  );
+};
+
+export default mainRouter;
