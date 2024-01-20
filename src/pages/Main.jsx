a0fefdde4bff9309afa161d7f83be968f8ee2546
KANGYONGSU23 <edc20203@gmail.com>
2024-01-20 23:24:43 +0900

    🔥 :: 테스트 코드 정리

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,36 +1,7 @@
-import { useForm } from 'react-hook-form';
-import Input from '../common/Input';
-import { useEffect } from 'react';
-
 const MainPage = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      input: '',
-    },
-  });
-
-  useEffect(() => {
-    console.log(errors);
-  }, [errors]);
-
   return (
     <>
-      <form onSubmit={handleSubmit(console.log)}>
-        <Input
-          {...register('input', {
-            required: '필수 값 입니다.',
-            maxLength: {
-              value: 5,
-              message: '최대값을 5 입니다',
-            },
-          })}
-          placeholder="제목 작성하기"
-        />
-      </form>
+      <h1>MainPage</h1>
     </>
   );
 };
