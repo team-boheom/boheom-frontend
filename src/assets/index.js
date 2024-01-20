b7d322aa6cf0354bae5337e94acf6eba3822dfdf
juwon <juwon2298@naver.com>
2024-01-21 01:19:56 +0900

    임시커밋

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
 export { default as Logo } from './Logo.svg';
+export { default as Arrow } from './Arrow.svg';
+export { default as Date } from './Date.svg';
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,9 +1,59 @@
-const CreatePostPage = () => {
+import React from 'react';
+import styled from '@emotion/styled';
+import { Arrow, Date } from '../assets';
+
+const CreatePost = () => {
   return (
     <>
-      <h1>CreatePostPage</h1>
+      {/* <Header /> */}
+      <Wrapper>
+        <TitleContainer>
+          <ArrowImg src={Arrow} />
+          <TitleWrapper>
+            <TitleRed>모집글 작성</TitleRed>
+            <TitleBlack>모험을 시작해볼까요?</TitleBlack>
+          </TitleWrapper>
+        </TitleContainer>
+      </Wrapper>
     </>
   );
 };
 
-export default CreatePostPage;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 380px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 130px;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const ArrowImg = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const TitleBasic = styled.div`
+  font-weight: ${({ theme }) => theme.weight.bold};
+  font-size: ${({ theme }) => theme.fontSize.header1};
+`;
+
+const TitleRed = styled(TitleBasic)`
+  color: ${({ theme }) => theme.color.red};
+  padding-bottom: 5px;
+`;
+
+const TitleBlack = styled(TitleBasic)`
+  color: ${({ theme }) => theme.color.black};
+`;
+
+export default CreatePost;
