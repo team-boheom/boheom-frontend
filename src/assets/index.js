f7a5f9e6e84bddc27b49b60d44502774700e80ed
KANGYONGSU23 <edc20203@gmail.com>
2024-01-22 19:30:41 +0900

    ✨ :: PostCard 퍼블리싱

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,4 @@
 export { default as Logo } from './Logo.svg';
 export { default as Search } from './Search.svg';
+export { default as Person } from './Person.svg';
+export { default as View } from './View.svg';
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,117 @@
+import styled from '@emotion/styled';
+import { Person, View } from '../assets';
+import { Link } from 'react-router-dom';
+
+const PostCard = ({
+  tags,
+  title,
+  content,
+  view,
+  recruitment,
+  apply_count,
+  id,
+}) => {
+  return (
+    <Container to={`/post/${id}`}>
+      <div>
+        {tags.map((item) => (
+          <Tag>#{item}</Tag>
+        ))}
+      </div>
+      <TextContents>
+        <div className="title_">
+          <p>{title}</p>
+        </div>
+        <div className="main_text">
+          <p>{content}</p>
+        </div>
+      </TextContents>
+      <EtcInfo>
+        <div>
+          <img src={View} alt="조횟수" />
+          <p>{view}</p>
+        </div>
+        <div>
+          <img src={Person} alt="신청인원" />
+          <p>
+            {apply_count}/{recruitment}
+          </p>
+        </div>
+      </EtcInfo>
+    </Container>
+  );
+};
+
+const Container = styled(Link)`
+  width: 100%;
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: 8px;
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.05);
+  padding: 20px 24px;
+  cursor: pointer;
+  transition: 0.2s all;
+  :hover {
+    scale: 1.02;
+  }
+`;
+
+const Tag = styled.span`
+  margin-right: 4px;
+  ${({ theme }) => ({
+    fontSize: theme.fontSize.caption,
+    fontWeight: theme.fontWeight.reguler,
+    color: theme.color.green600,
+  })}
+`;
+
+const TextContents = styled.div`
+  > div {
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+  }
+  > .title_ {
+    max-height: 40px;
+    margin-bottom: 12px;
+    margin-top: 8px;
+    > p {
+      ${({ theme }) => ({
+        fontSize: theme.fontSize.body1,
+        fontWeight: theme.fontWeight.bold,
+        color: theme.color.black,
+      })}
+    }
+  }
+  > .main_text {
+    max-height: 28px;
+    > p {
+      ${({ theme }) => ({
+        fontSize: theme.fontSize.body2,
+        fontWeight: theme.fontWeight.reguler,
+        color: theme.color.gray500,
+      })}
+    }
+  }
+`;
+
+const EtcInfo = styled.div`
+  display: flex;
+  gap: 12px;
+  margin-top: 14px;
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+  }
+  p {
+    ${({ theme }) => ({
+      fontSize: theme.fontSize.caption,
+      fontWeight: theme.fontWeight.reguler,
+      color: theme.color.gray500,
+    })}
+  }
+`;
+
+export default PostCard;
