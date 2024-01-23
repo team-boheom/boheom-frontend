807a085281f26b64fa3fcb258a57d1d5badac4cb
한예슬 <yangsooh386@gmail.com>
2024-01-23 22:24:40 +0900

    💄 :: 폰트 굵기&크기 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -78,6 +78,10 @@ const StyledLink = styled(Link)`
   text-decoration: underline;
   text-underline-position: under;
   margin-top: 50px;
+  ${({ theme }) => ({
+    fontWeight: theme.fontWeight.regular,
+    fontSize: theme.fontSize.body2,
+  })}
 `;
 
 export default Login;
