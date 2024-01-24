d20db327ef9e7f3cd7e4732afbfba4e5880e4fd7
KANGYONGSU23 <edc20203@gmail.com>
2024-01-24 19:42:05 +0900

    ✨ :: interceptor 설정

---
+++ src/utils/axios.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+const localStorage = window.localStorage;
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('access_token');
+    const returnConfig = { ...config };
+    if (accessToken) {
+      returnConfig.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return returnConfig;
+  },
+  (error) => error
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (axios.isAxiosError(error) && error.response) {
+      if (
+        error.response.status === 403 ||
+        error.response.status === 401 ||
+        error.response.data.message === 'User Not Found'
+      ) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+
+        toast.error('로그인이 만료되었습니다. 다시 로그인해주세요.');
+
+        const goToLogin = setTimeout(() => {
+          console.log('이동');
+          window.location.href = '/login';
+          clearTimeout(goToLogin);
+        }, 1000);
+      }
+    }
+    return error;
+  }
+);
