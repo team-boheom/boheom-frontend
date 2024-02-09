efe5a662c026af72d7eb1ca688d27b0d6ee22e87
주원 <juwon2298@naver.com>
2024-02-10 04:05:07 +0900

    🔥 :: 회원가입 페이지 api 연동 및 useForm 리팩토링

--- src/utils/api/auth.js
+++ src/utils/api/auth.js
@@ -34,3 +34,36 @@ export const useLogin = () => {
     }
   );
 };
+
+export const useSignup = () => {
+  const navigate = useNavigate();
+  return useMutation(
+    async (params) => {
+      return await instance.post('/users/signup', params);
+    },
+    {
+      onError: (error) => {
+        switch (error?.response?.status) {
+          case 401:
+            toast.error('인증애 실패했습니다.');
+            break;
+          case 403:
+            toast.error('사용자 권한이 없습니다.');
+            break;
+          case 404:
+            toast.error('유저 정보가 일치하지 않습니다.');
+            break;
+          case 409:
+            toast.error('이미 가입된 유저 입니다!');
+            break;
+          default:
+            toast.error('오류가 발생하였습니다.');
+        }
+      },
+      onSuccess: () => {
+        toast.success('회원가입에 성공했습니다.');
+        navigate('/login');
+      },
+    }
+  );
+};
